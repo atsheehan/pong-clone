99cb234a008ccbe28253d0687502b99720e5ee1f
Adam Sheehan <a.t.sheehan@gmail.com>
2010-03-23 12:17:43 -0700

    added collision detection for the two paddles.
    still need to consider deflection off the top or bottom of the paddle

--- src/ball.cpp
+++ src/ball.cpp
@@ -17,24 +17,52 @@ void Ball::init(int start_x, int start_y) {
   x_speed = y_speed = 0;
 }
 
-void Ball::update_position(const SDL_Rect& playing_field, const SDL_Rect& first_paddle, const SDL_Rect& second_paddle) {
+void Ball::update_position(const SDL_Rect& playing_field, const SDL_Rect& left_paddle, const SDL_Rect& right_paddle) {
 
   position.x += x_speed;
   position.y += y_speed;
   
+  // these calculated constants make reading the collision detection much easier
+  const int playing_field_bottom_edge = playing_field.y + playing_field.h;
+  const int ball_bottom_edge = position.y + position.h;
+  const int ball_right_edge = position.x + position.w;
+  const int left_paddle_right_edge = left_paddle.x + left_paddle.w;
+  const int left_paddle_bottom_edge = left_paddle.y + left_paddle.h;
+  const int right_paddle_right_edge = right_paddle.x + right_paddle.w;
+  const int right_paddle_bottom_edge = right_paddle.y + right_paddle.h;
+  
+
+  // check for collision with the top and bottom of the playing field
   if (position.y < playing_field.y) {
     int difference = playing_field.y - position.y;
     position.y = playing_field.y + difference;
     y_speed = -y_speed;
   }
 
-  const int playing_field_bottom_edge = playing_field.y + playing_field.h;
-  const int ball_bottom_edge = position.y + position.h;
-
   if (ball_bottom_edge > playing_field_bottom_edge) {
     int difference = ball_bottom_edge - playing_field_bottom_edge;
     position.y = playing_field_bottom_edge - position.h - difference;
     y_speed = -y_speed;
+  }
+
+  // check for collision with the left paddle
+  if (position.x < left_paddle_right_edge &&
+      ball_right_edge > left_paddle.x &&
+      position.y < left_paddle_bottom_edge &&
+      ball_bottom_edge > left_paddle.y) {
+    int difference = left_paddle_right_edge - position.x;
+    position.x = left_paddle_right_edge + difference;
+    x_speed = -x_speed;
+  }
+
+  // check for collision with the right paddle
+  if (ball_right_edge > right_paddle.x &&
+      position.x < right_paddle_right_edge &&
+      position.y < right_paddle_bottom_edge &&
+      ball_bottom_edge > right_paddle.y) {
+    int difference = ball_right_edge - right_paddle.x;
+    position.x = right_paddle.x - position.w - difference;
+    x_speed = -x_speed;
   }
 }
 
--- test/ball_test.cpp
+++ test/ball_test.cpp
@@ -14,21 +14,21 @@ namespace {
       playing_field.w = 600;
       playing_field.h = 300;
 
-      first_paddle.x = 40;
-      second_paddle.x = 480;
-      first_paddle.y = 50;
-      second_paddle.y = 50;
+      left_paddle.x = 40;
+      right_paddle.x = 480;
+      left_paddle.y = 50;
+      right_paddle.y = 50;
       
-      first_paddle.w = second_paddle.w = 20;
-      first_paddle.h = second_paddle.h = 60;
+      left_paddle.w = right_paddle.w = 20;
+      left_paddle.h = right_paddle.h = 60;
     }
 
     ~BallTestFixture() {
     }
     
     SDL_Rect playing_field;
-    SDL_Rect first_paddle;
-    SDL_Rect second_paddle;
+    SDL_Rect left_paddle;
+    SDL_Rect right_paddle;
   };
 
   // although not entirely useful, if creating a ball object with no arguments
@@ -63,7 +63,7 @@ namespace {
     
     // if the ball has no speed, it should remain in the same spot after an update
     SDL_Rect original_position = ball.get_position();
-    ball.update_position(playing_field, first_paddle, second_paddle);
+    ball.update_position(playing_field, left_paddle, right_paddle);
     SDL_Rect new_position = ball.get_position();
 
     CHECK(original_position.x == new_position.x);
@@ -73,17 +73,17 @@ namespace {
   // when the ball is assigned a velocity, verify it moves based on the values given
   // each time the update is called.
   TEST_FIXTURE(BallTestFixture, assigning_a_velocity_to_the_ball_should_enable_it_to_move_during_an_update) {
-    // set the ball at (0,0) and give is an x speed of 10 and a y speed of 20
+    // set the ball at (0,0) and give is an x speed of 1 and a y speed of 2
     Ball ball = Ball();
-    ball.set_speed(10, 20);
+    ball.set_speed(1, 2);
 
-    // calling update twice should set the position at (2*10, 2*20)
-    ball.update_position(playing_field, first_paddle, second_paddle);
-    ball.update_position(playing_field, first_paddle, second_paddle);
+    // calling update twice should set the position at (2*1, 2*2)
+    ball.update_position(playing_field, left_paddle, right_paddle);
+    ball.update_position(playing_field, left_paddle, right_paddle);
     
     SDL_Rect new_position = ball.get_position();
-    CHECK(new_position.x == 20);
-    CHECK(new_position.y == 40);
+    CHECK(new_position.x == 2);
+    CHECK(new_position.y == 4);
   }
 
   // a negative speed should move the ball in the opposite direction
@@ -91,8 +91,8 @@ namespace {
     Ball ball = Ball(100, 200);
     ball.set_speed(-20, -40);
 
-    ball.update_position(playing_field, first_paddle, second_paddle);
-    ball.update_position(playing_field, first_paddle, second_paddle);
+    ball.update_position(playing_field, left_paddle, right_paddle);
+    ball.update_position(playing_field, left_paddle, right_paddle);
 
     SDL_Rect new_position = ball.get_position();
     CHECK(new_position.x == 60);
@@ -110,12 +110,12 @@ namespace {
     // of the wall.
     ball.set_speed(0, -12);
 
-    ball.update_position(playing_field, first_paddle, second_paddle);
+    ball.update_position(playing_field, left_paddle, right_paddle);
     SDL_Rect updated_position = ball.get_position();
 
     CHECK(updated_position.y == (playing_field.y + 7));
 
-    ball.update_position(playing_field, first_paddle, second_paddle);
+    ball.update_position(playing_field, left_paddle, right_paddle);
     updated_position = ball.get_position();
 
     CHECK(updated_position.y == (playing_field.y + 19));
@@ -129,14 +129,130 @@ namespace {
     
     ball.set_speed(0, 12);
 
-    ball.update_position(playing_field, first_paddle, second_paddle);
+    ball.update_position(playing_field, left_paddle, right_paddle);
     SDL_Rect updated_position = ball.get_position();
 
     CHECK(updated_position.y == (playing_field.y + playing_field.h - Ball::DefaultDiameter - 7));
 
-    ball.update_position(playing_field, first_paddle, second_paddle);
+    ball.update_position(playing_field, left_paddle, right_paddle);
     updated_position = ball.get_position();
 
     CHECK(updated_position.y == (playing_field.y + playing_field.h - Ball::DefaultDiameter - 19));
+  }
+  
+  // when the ball hits the front of the left paddle, it should deflect back with the same
+  // speed.
+  TEST_FIXTURE(BallTestFixture, deflect_off_front_of_left_paddle) {
+    // start the ball 5 units away from the front of the left (first) paddle
+    const int right_edge_of_paddle = left_paddle.x + left_paddle.w;
+    int ball_start_x = right_edge_of_paddle + 5;
+    Ball ball(ball_start_x, left_paddle.y);
+
+    // if the ball is moving towards the paddle at 12 units, it should deflect back after
+    // hitting it at 5 units and then move 7 units away from the start. after another update
+    // it should move 12 units further away and end up at 19 units from the paddle.
+    ball.set_speed(-12, 0);
+    ball.update_position(playing_field, left_paddle, right_paddle);
+    ball.update_position(playing_field, left_paddle, right_paddle);
+
+    SDL_Rect new_position = ball.get_position();
+    CHECK(new_position.x == right_edge_of_paddle + 19);
+  }
+
+  // when testing for a collision with the paddle, need to check whether the x-value extends
+  // past the right edge of the paddle as well as checking whether the y-values are within the
+  // bounds of the paddle.
+  TEST_FIXTURE(BallTestFixture, move_past_left_paddle_if_not_within_y_coordinates) {
+    // start the ball 5 units away from the front of the left paddle, but move it
+    // so that it should avoid hitting the bottom of the paddle
+    const int right_edge_of_paddle = left_paddle.x + left_paddle.w;
+    const int bottom_edge_of_paddle = left_paddle.y + left_paddle.h;
+    Ball ball(right_edge_of_paddle + 5, bottom_edge_of_paddle);
+
+    // the ball should move past the paddle without colliding or changing direction. starting
+    // at 5 units to the right of the paddle, after updating twice at a speed of 12 it should be
+    // (5 - 2*12) = -19 units away from the right edge of the paddle
+    ball.set_speed(-12, 0);
+    ball.update_position(playing_field, left_paddle, right_paddle);
+    ball.update_position(playing_field, left_paddle, right_paddle);
+
+    SDL_Rect new_position = ball.get_position();
+    CHECK(new_position.x == right_edge_of_paddle - 19);    
+  }
+
+  // if the ball moves past the paddle and is moving up behind it, it should not deflect off 
+  // anything.
+  TEST_FIXTURE(BallTestFixture, move_behind_left_paddle) {
+    const int paddle_bottom_edge = left_paddle.y + left_paddle.h;
+    const int ball_start_x = left_paddle.x - Ball::DefaultDiameter;
+    Ball ball(ball_start_x, paddle_bottom_edge + 5);
+
+    // move the ball up behind the paddle at 12 units / update while moving slightly to the left
+    // at 1 unit/update. after two updates, the top edge of the ball should be 19 units past the
+    // bottom edge of the paddle.
+    ball.set_speed(-1, -12);
+    ball.update_position(playing_field, left_paddle, right_paddle);
+    ball.update_position(playing_field, left_paddle, right_paddle);
+    
+    SDL_Rect new_position = ball.get_position();
+    CHECK(new_position.y == paddle_bottom_edge - 19);
+    CHECK(new_position.x == ball_start_x - 2);
+  }
+
+  // when the ball hits the front of the right paddle, it should deflect back with the same
+  // speed.
+  TEST_FIXTURE(BallTestFixture, deflect_off_front_of_right_paddle) {
+    // start the ball 5 units away from the front of the right paddle
+    int ball_start_x = right_paddle.x - Ball::DefaultDiameter - 5;
+    Ball ball(ball_start_x, right_paddle.y);
+
+    // if the ball is moving towards the paddle at 12 units, it should deflect back after
+    // hitting it at 5 units and then move 7 units away from the start. after another update
+    // it should move 12 units further away and end up at 19 units from the paddle.
+    ball.set_speed(12, 0);
+    ball.update_position(playing_field, left_paddle, right_paddle);
+    ball.update_position(playing_field, left_paddle, right_paddle);
+
+    SDL_Rect new_position = ball.get_position();
+    CHECK(new_position.x == right_paddle.x - Ball::DefaultDiameter - 19);
+  }
+
+  // when testing for a collision with the paddle, need to check whether the x-value extends
+  // past the edge of the paddle as well as checking whether the y-values are within the
+  // bounds of the paddle.
+  TEST_FIXTURE(BallTestFixture, move_past_right_paddle_if_not_within_y_coordinates) {
+    // start the ball 5 units away from the front of the right paddle, but move it
+    // so that it should avoid hitting the bottom of the paddle
+    int ball_start_x = right_paddle.x - Ball::DefaultDiameter - 5;
+    Ball ball(ball_start_x, right_paddle.y + right_paddle.h);
+
+    // the ball should move past the paddle without colliding or changing direction. starting
+    // at 5 units away from the edge of the paddle, after updating twice at a speed of 12 the right
+    // edge of the ball  should be (-5 + 2*12) = 19 units away from the left edge of the paddle
+    ball.set_speed(12, 0);
+    ball.update_position(playing_field, left_paddle, right_paddle);
+    ball.update_position(playing_field, left_paddle, right_paddle);
+
+    SDL_Rect new_position = ball.get_position();
+    CHECK(new_position.x + new_position.w == right_paddle.x + 19);    
+  }
+
+  // if the ball moves past the paddle and is moving up behind it, it should not deflect off 
+  // anything.
+  TEST_FIXTURE(BallTestFixture, move_behind_right_paddle) {
+    const int paddle_bottom_edge = right_paddle.y + right_paddle.h;
+    const int ball_start_x = right_paddle.x + right_paddle.w;
+    Ball ball(ball_start_x, paddle_bottom_edge + 5);
+
+    // move the ball up behind the paddle at 12 units / update while moving slightly to the right
+    // at 1 unit/update. after two updates, the top edge of the ball should be 19 units past the
+    // bottom edge of the paddle.
+    ball.set_speed(1, -12);
+    ball.update_position(playing_field, left_paddle, right_paddle);
+    ball.update_position(playing_field, left_paddle, right_paddle);
+    
+    SDL_Rect new_position = ball.get_position();
+    CHECK(new_position.y == paddle_bottom_edge - 19);
+    CHECK(new_position.x == ball_start_x + 2);
   }
 }
